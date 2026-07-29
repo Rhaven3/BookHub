19f7f4c406e1796df49ae2b1b6e8a922d20245c6
io2944 <dumdia79@gmail.com>
2026-07-29 15:12:18 +0200

    x

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { AUTH_ROUTES } from './features/auth/auth.routes';
 import { PROFILE_ROUTES } from './features/account/account.routes';
 import { AUTHOR_ROUTES } from './features/author/author.routes';
 import { BOOK_ROUTES } from './features/book/book.routes';
+import { authGuard } from './core/guards/auth/auth-guard';
+import { DASHBOARD_ROUTES } from './features/dashboard/dashboard.routes';
+import { adminGuard } from './core/guards/admin/admin-guard';
 
 export const routes: Routes = [
   { path: 'image', component: MainLayout, children: IMAGE_ROUTES, canActivate: [authGuard] },
@@ -13,5 +16,5 @@ export const routes: Routes = [
   { path: 'auth', component: MainLayout, children: AUTH_ROUTES },
   { path: 'author', component: MainLayout, children: AUTHOR_ROUTES, canActivate: [authGuard] },
   { path: 'book', component: MainLayout, children: BOOK_ROUTES, canActivate: [authGuard] },
-  { path: '', redirectTo: '/images', pathMatch: 'full' },
+  { path: '', redirectTo: '/image', pathMatch: 'full' },
 ];
