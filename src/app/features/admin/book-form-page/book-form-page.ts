34e397c4d40bf6411a14313c2ce4beeca3bda678
julienM29 <137764146+julienM29@users.noreply.github.com>
2026-07-30 16:58:20 +0200

    x

--- src/app/features/admin/book-form-page/book-form-page.ts
+++ src/app/features/admin/book-form-page/book-form-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BookForm } from './component/book-form/book-form';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-form-page',
-  imports: [BookForm],
+  imports: [BookForm, RouterLink],
   templateUrl: './book-form-page.html',
   styleUrl: './book-form-page.css',
 })
